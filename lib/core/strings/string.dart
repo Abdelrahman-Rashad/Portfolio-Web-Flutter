48903e61944786511f1853674943cea1898f04b3
Rashad-Bayanatz <a.rashad@bayanatz.com>
2025-05-01 19:16:59 +0300

    update CV link in Strings class

--- lib/core/strings/string.dart
+++ lib/core/strings/string.dart
@@ -5,5 +5,5 @@ class Strings {
   static const String behance = "https://www.behance.net/abdelrarashad9";
   static const String gmail = "abdelrahman.a.rashad@gmail.com";
   static const String CV =
-      "https://drive.google.com/file/d/1z_hMknkbYdQX4KryC6UsO31EsTBKDI3c/view?usp=sharing";
+      "https://drive.google.com/file/d/1Rqu1Oe9NZSHmblMf4jOeVD4FFiu_hESm/view?usp=sharing";
 }
